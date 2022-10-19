060ddc78cc4bccc41183527b05b3a24201725d3e
Chobarrrr <i201796@nu.edu.pk>
2022-10-20 02:09:18 +0500

    "React Commit 4 "

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -9,7 +9,7 @@ export default function home() {
       >
         <div class="container">
           <a class="navbar-brand" href="index.html">
-            Furni<span>.</span>
+            Amoire<span>.</span>
           </a>
 
           <button
@@ -90,9 +90,6 @@ export default function home() {
         <div class="container">
           <div class="row mb-5">
             <div class="col-md-12">
-              <div class="border p-4 rounded" role="alert">
-                Returning customer?<a href="#">Click here</a> to login
-              </div>
             </div>
           </div>
           <div class="row">
@@ -600,57 +597,19 @@ export default function home() {
             <img src="assests/images/sofa.png" alt="Image" class="img-fluid" />
           </div>
 
-          <div class="row">
-            <div class="col-lg-8">
-              <div class="subscription-form">
-                <h3 class="d-flex align-items-center">
-                  <span class="me-1">
-                    <img
-                      src="assests/images/envelope-outline.svg"
-                      alt="Image"
-                      class="img-fluid"
-                    />
-                  </span>
-                  <span>Subscribe to Newsletter</span>
-                </h3>
-
-                <form action="#" class="row g-3">
-                  <div class="col-auto">
-                    <input
-                      type="text"
-                      class="form-control"
-                      placeholder="Enter your name"
-                    />
-                  </div>
-                  <div class="col-auto">
-                    <input
-                      type="email"
-                      class="form-control"
-                      placeholder="Enter your email"
-                    />
-                  </div>
-                  <div class="col-auto">
-                    <button class="btn btn-primary">
-                      <span class="fa fa-paper-plane"></span>
-                    </button>
-                  </div>
-                </form>
-              </div>
-            </div>
-          </div>
-
+         
           <div class="row g-5 mb-5">
             <div class="col-lg-4">
               <div class="mb-4 footer-logo-wrap">
                 <a href="#" class="footer-logo">
-                  Furni<span>.</span>
+                  Amoire<span>.</span>
                 </a>
               </div>
               <p class="mb-4">
-                Donec facilisis quam ut purus rutrum lobortis. Donec vitae odio
-                quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam
-                vulputate velit imperdiet dolor tempor tristique. Pellentesque
-                habitant
+                A Company that wants to provide the best 
+                for its customer. We belive in that it is  
+                the customer how are the essance of a bussiness 
+                and the key to success. 
               </p>
 
               <ul class="list-unstyled custom-social">
@@ -750,8 +709,7 @@ export default function home() {
                 <p class="mb-2 text-center text-lg-start">
                   Copyright &copy;
                   <script>document.write(new Date().getFullYear());</script>.All
-                  Rights Reserved.&mdash; Designed with love by{" "}
-                  <a href="https://untree.co">Untree.co</a>
+                  Rights Reserved.&mdash; Designed with love by Amoire.
                 </p>
               </div>
 
