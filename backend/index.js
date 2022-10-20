f2c28ef1961094aa848d282cc94a01db0fcf9af6
Chobarrrr <i201796@nu.edu.pk>
2022-10-20 06:00:49 +0500

    Model Finalized

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,35 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const CheckoutModel = require('./Models/Checkout')
+const CheckoutModel = require("./Models/Checkout");
+const cors = require("cors");
+
 mongoose.connect(
- "mongodb+srv://nolifenoodler:nolife_12345678@checkout.nqorw3h.mongodb.net/Checkout?retryWrites=true&w=majority"
+  "mongodb+srv://nolifenoodler:nolife_12345678@checkout.nqorw3h.mongodb.net/Checkout?retryWrites=true&w=majority"
 );
+
 const port = 3001;
 
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
 
-app.get("/getBillingInfo",(req,res) => {
-    CheckoutModel.find({}, (err,result) =>{
-        if (err){
-            res.json(err)
-        }
-        else{
-            res.json(result)
-        }
-    } )
-})
+app.get("/getBillingInfo", (req, res) => {
+  CheckoutModel.find({}, (err, result) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(result);
+    }
+  });
+});
 
 app.post("/addBillingInfo", async (req, res) => {
-    const billInfo = req.body
-    const newBill = new CheckoutModel(billInfo)
-    await newBill.save()
+  const billInfo = req.body;
+  const newBill = new CheckoutModel(billInfo);
+  await newBill.save();
+  res.json(newBill);
+});
 
-    res.json(newBill)
-
-})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
